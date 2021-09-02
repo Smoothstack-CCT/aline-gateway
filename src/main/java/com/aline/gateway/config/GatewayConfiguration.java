8260dba1037975363d4e4b018c27b603895eae26
leandroyabut <leandro.yabut@smoothstack.com>
2021-09-02 13:26:37 -0600

    Add gateway configuration properties.

--- src/main/java/com/aline/gateway/config/GatewayConfiguration.java
+++ src/main/java/com/aline/gateway/config/GatewayConfiguration.java
@@ -1,22 +1,43 @@
 package com.aline.gateway.config;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.GatewayFilterSpec;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.gateway.route.builder.UriSpec;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 @Configuration
+@Slf4j
+@RequiredArgsConstructor
 public class GatewayConfiguration {
+
+    private final GatewayConfigurationProperties gatewayProperties;
 
     @Bean
     public RouteLocator routeLocator(RouteLocatorBuilder builder) {
-        return builder
-                .routes()
-                .route("user-microservice", r -> r
-                        .path("/api/users", "/api/users/{segment}", "/login")
-                        .filters(f -> f.rewritePath("/api/(?<segment>.*)", "/${segment}"))
-                        .uri("http://localhost:8070"))
-                .build();
+        RouteLocatorBuilder.Builder routes = builder.routes();
+
+        final String pathPrefix = gatewayProperties.getPathPrefix();
+        final String serviceHost = gatewayProperties.getServiceHost();
+
+        final Function<GatewayFilterSpec, UriSpec> rewritePath = r -> r.rewritePath(pathPrefix + "/(?<segment>.*)", "/${segment}");
+
+        gatewayProperties.getRoutes().forEach(route -> routes.route(route.getId(),
+                r -> r.path(route.getPaths().stream().map(path -> path.replace("@", pathPrefix))
+                                    .collect(Collectors.toList())
+                                    .toArray(new String[0]))
+                        .filters(rewritePath)
+                        .uri(serviceHost + ":" + route.getPort())
+                )
+        );
+
+        return routes.build();
     }
 
 }
--- src/main/java/com/aline/gateway/config/GatewayConfigurationProperties.java
+++ src/main/java/com/aline/gateway/config/GatewayConfigurationProperties.java
@@ -1,9 +1,42 @@
 package com.aline.gateway.config;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
+@Getter
+@Setter
 @Configuration
 @ConfigurationProperties(prefix = "app.gateway")
 public class GatewayConfigurationProperties {
+
+    /**
+     * Routes for the gateway to map to.
+     */
+    private List<Route> routes;
+    private String serviceHost;
+    private String pathPrefix;
+
+    @Getter
+    @Setter
+    @ToString
+    static class Route {
+        /**
+         * ID of the route (typically the microservice name)
+         */
+        private String id;
+        /**
+         * The port of the microservice
+         */
+        private int port;
+        /**
+         * The paths to map to this microservice
+         */
+        private List<String> paths;
+    }
+
 }
