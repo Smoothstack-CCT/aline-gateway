8be5af34f1db142f1da9c1d2944eb21336abd820
leandroyabut <leandro.yabut@smoothstack.com>
2021-09-14 22:09:21 -0600

    Fix tests failing.

--- src/test/java/com/aline/gateway/AlineGatewayApplicationTests.java
+++ src/test/java/com/aline/gateway/AlineGatewayApplicationTests.java
@@ -2,8 +2,10 @@ package com.aline.gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AlineGatewayApplicationTests {
 
     @Test
