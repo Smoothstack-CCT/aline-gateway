b16a075c1187ae1723e5ff7dc5d1e2ad6ef98ca5
Kevin Lyons <kevin.lyons@smoothstack.com>
2022-04-22 11:27:43 -0700

    CB-17 dockerized gateway

--- pythonscripts/userdata/userdata.py
+++ pythonscripts/userdata/userdata.py
@@ -1,16 +1,12 @@
 """ creates user information to populate the database """
 
-
-
 import random
 from faker import Faker
 
 import sys
 sys.path.insert(
     0, '/Users/kevinlyons/Documents/code/capstone/aline-gateway-KDL/pythonscripts/applicationsandapplicants')
-
 from applicantsandapplicationsroutes import application_generator_checking_and_savings_api
-
 
 def create_user_data_admin():
     """ profile creator function """
@@ -21,12 +17,9 @@ def create_user_data_admin():
 
     first_name = fake.first_name()
     last_name = fake.last_name()
-    
 
     full_name = first_name+"."+last_name
     email = full_name + "@gmail.com"
-
-
 
     password = "P@ssword1!"
     phone = "{}-{}-{}".format(three_digit, three_digit, four_digit)
@@ -40,6 +33,7 @@ def create_user_data_admin():
 def create_user_data_member():
     data = application_generator_checking_and_savings_api()
     data = data.json()
+    print(data)
     created_members = data.get('createdMembers')
     membership_id = created_members[0]
     name = membership_id.get('name')
@@ -47,19 +41,15 @@ def create_user_data_member():
     social_security = data.get('applicants')
     social_security = social_security[0]
     social_security = social_security.get('socialSecurity').split('-')
-    
 
     username = str(name.replace(' ', '.'))
-    
-    last_four_ssn = social_security[2]
 
-    
+    last_four_ssn = social_security[2]
 
     new_member = {"role": "member", "username": username, "password": "P@ssword1!",
                   "membershipId": membership_id, "lastFourOfSSN": last_four_ssn}
 
-
     return new_member
 
 
-
+create_user_data_member()
